cdac99475facf19a30fae97c9ac025417574f548
Serco <xg.chen87@gmail.com>
2015-11-11 01:13:56 +0800

    add other update methods for bucket

--- lib/oss/base.rb
+++ lib/oss/base.rb
@@ -10,8 +10,14 @@ module OSS
       )
     end
 
-    def_delegators :bucket, :get_service, :put_bucket, :put_bucket_acl,
-      :put_bucket_logging
+    def_delegators :bucket,
+                   :get_service,
+                   :put_bucket,
+                   :put_bucket_acl,
+                   :put_bucket_logging,
+                   :put_bucket_website,
+                   :put_bucket_referer,
+                   :put_bucket_lifecycle
 
     private
 
--- lib/oss/bucket.rb
+++ lib/oss/bucket.rb
@@ -32,7 +32,7 @@ module OSS
       client.run :put, '?acl', nil, headers
     end
 
-    def put_bucket_logging(source, target=nil, prefix=nil)
+    def put_bucket_logging(source, target = nil, prefix = nil)
       body = '<?xml version="1.0" encoding="UTF-8"?><BucketLoggingStatus>'
       if target
         body += "<LoggingEnabled><TargetBucket>#{target}</TargetBucket>"
@@ -40,11 +40,89 @@ module OSS
         body += '</LoggingEnabled>'
       end
       body += '</BucketLoggingStatus>'
+
       headers = {
         'Content-Type' => 'application/xml'
       }
       setup_bucket_options(source, 'logging')
       client.run :put, '?logging', body, headers
+    end
+
+    def put_bucket_website(name, index, error)
+      body = '<?xml version="1.0" encoding="UTF-8"?><WebsiteConfiguration>' \
+             "<IndexDocument><Suffix>#{index}</Suffix></IndexDocument>"
+      if error
+        body += "<ErrorDocument><Key>#{error}</Key></ErrorDocument>"
+      end
+      body += '</WebsiteConfiguration>'
+
+      headers = {
+        'Content-Type' => 'application/xml'
+      }
+
+      setup_bucket_options(name, 'website')
+      client.run :put, '?website', body, headers
+    end
+
+    def put_bucket_website(name, index, error)
+      body = '<?xml version="1.0" encoding="UTF-8"?><WebsiteConfiguration>' \
+             "<IndexDocument><Suffix>#{index}</Suffix></IndexDocument>"
+      if error
+        body += "<ErrorDocument><Key>#{error}</Key></ErrorDocument>"
+      end
+      body += '</WebsiteConfiguration>'
+
+      headers = {
+        'Content-Type' => 'application/xml'
+      }
+
+      setup_bucket_options(name, 'website')
+      client.run :put, '?website', body, headers
+    end
+
+    def put_bucket_referer(name, allow_empty_referer, referers=[])
+      body = '<?xml version="1.0" encoding="UTF-8"?><RefererConfiguration>' \
+             "<AllowEmptyReferer>#{allow_empty_referer}</AllowEmptyReferer >" \
+             "<RefererList>"
+      referers.each do |referer|
+        body += "<Referer>#{referer}</Referer>"
+      end
+      body += '</RefererList></RefererConfiguration>'
+
+      headers = {
+        'Content-Type' => 'application/xml'
+      }
+
+      setup_bucket_options(name, 'referer')
+      client.run :put, '?referer', body, headers
+    end
+
+    # status: Enabled, Disabled
+    def put_bucket_lifecycle(name, rules=[])
+      body = '<?xml version="1.0" encoding="UTF-8"?><LifecycleConfiguration>'
+      rules.each do |rule|
+        rule_content = '<Rule>'
+        rule_content += "<ID>#{rule[:id]}</ID>" if rule[:id]
+        rule_content += "<Prefix>#{rule[:prefix]}</Prefix>" if rule[:prefix]
+        rule_content += "<Status>#{rule[:status]}</Status>"
+        expiration =
+          if rule[:days]
+            "<Days>#{rule[:days]}</Days>"
+          else
+            "<Date>#{rule[:date]}</Date>"
+          end
+        rule_content += "<Expiration>#{expiration}</Expiration></Rule>"
+        body += rule_content
+      end
+
+      body += '</LifecycleConfiguration>'
+
+      headers = {
+        'Content-Type' => 'application/xml'
+      }
+
+      setup_bucket_options(name, 'lifecycle')
+      client.run :put, '?lifecycle', body, headers
     end
 
     private
